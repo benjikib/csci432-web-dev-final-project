5d1dd3b99c12f19fe88867d600feab7df8936b04
Michael White <mvwhite01@wm.edu>
2025-09-22 12:41:23 -0400

    Add components for SPA routing

---
+++ src/components/Motions.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+function Motions() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <main id="main">
+        <div id="side-bar">
+            Side 
+        </div>
+        <div id="motions-container">
+            <div class="motions-section">
+            <h2 class="section-title">Motions</h2>
+            <div class="motions-grid">
+                <div class="motion-card">
+                <div class="row1">
+                    <a class="title" href="#">Motion #1</a>
+                </div>
+                <div class="row2">
+                    Description Text.
+                </div>
+                <div class="row3">
+                    Votes:
+                </div>
+                </div>
+                <div class="motion-card">
+                <div class="row1">
+                    <a class="title" href="#">Motion #2</a>
+                </div>
+                <div class="row2">
+                    Another description.
+                </div>
+                <div class="row3">
+                    Votes:
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </main>
+    </>
+  )
+}
+export default Motions
