5159c078bbbe94d951891123468b736fd98de770
Jeffery Lin <jlin20@wm.edu>
2025-09-23 01:31:17 -0400

    setup page for motion details

---
+++ src/components/MotionDetails.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+function MotionDetails() {
+
+  return (
+    <>
+        <div className="main-page">
+            <span className="main-logo-font">Commie</span>
+            <img src="/logo.png" alt="Logo" className="main-logo"></img>
+        </div>
+    </>
+  )
+}
+export default MotionDetails
