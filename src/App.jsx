1e0a0a1f5b37bac8152f4444929288c66c1f6db1
Benji Kiblinger <rbkiblinger@wm.edu>
2025-10-02 15:04:04 -0400

    update motion details page to follow figma design

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,37 @@ import LoginPage from './components/LoginPage.jsx'
 import MotionDetails from './components/MotionDetailsPage.jsx'
 import Settings from './components/SettingsPage.jsx'
 import Profile from './components/ProfilePage.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+function AppContent() {
+  const location = useLocation();
+  const background = location.state?.background;
+
+  return (
+    <>
+      <Routes location={background || location}>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/motions" element={<Motions />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/settings" element={<Settings />}></Route>
+        <Route path="/motiondetails/:id" element={<Motions />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route path="/motiondetails/:id" element={<MotionDetails />}></Route>
+        </Routes>
+      )}
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-          {/* <div className="topnav">
-            <a className="logo-link" href="/">
-              <span className="nav-logo-font">Commie</span>
-              <img src="/logo.png" alt="Logo" className="nav-logo"></img>
-            </a>
-            <div className="nav-right">
-              <a href="/motions" title="Notifications">Motions</a>
-              <a href="#notifications" title="Notifications">Notifications</a>
-              <a href="/settings" title="Settings">Settings</a>
-              <a href="/profile" title="Profile">Profile</a>
-            </div>
-          </div> */}
-          <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Main />}></Route>
-            <Route path="/motions" element={<Motions />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/settings" element={<Settings />}></Route>
-            <Route path="/motiondetails/:id" element={<MotionDetails />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-          </Routes>
-          </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   )
 }
 
